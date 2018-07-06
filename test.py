278c58b350c8cedb2157cf878954b192b6d0539c
Heeill(Floyd)Wang <heeillfloydwang@HeeillFloydWangs-MacBook-Pro.local>
2018-07-06 12:06:37 +0200

    basic operations

--- test.py
+++ test.py
@@ -4,4 +4,32 @@ connection = client.connect("http://localhost:4200/", username="crate")
 
 cursor = connection.cursor()
 
-cursor.execute("INSERT INTO tweets (id, text) VALUES (?,?)", ("floyd","hello world"))
+while True:
+    print("Pick operation:")
+    print("1 - INSERT")
+    print("2 - DELETE")
+    print("3 - SELECT")
+    print("4 - UPDATE")
+    print("0 - Terminate the program")
+    pick = int(input())
+
+    if pick == 0:
+        break
+    elif pick == 1:
+        cursor.execute("INSERT INTO newtable (id) VALUES (?)", ("floyd",))
+    elif pick == 2:
+        cursor.execute("DELETE FROM newtable WHERE id = ?", ("floyd",))
+    elif pick == 3:
+        cursor.execute("SELECT * FROM newtable WHERE id = ?", ("floyd",))
+    elif pick == 4:
+        cursor.execute("UPDATE newtable SET id = ? WHERE id = ?", ("pink", "floyd"))
+
+    result = cursor.fetchone()
+    print(result)
+    print(cursor.description)
+
+
+
+
+
+
